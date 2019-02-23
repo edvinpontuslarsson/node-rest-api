bf63f2f21f83aa3bec45b26f386f41c0ed0c57c0
Edvin Larsson <edvinpontuslarsson@gmail.com>
2019-02-23 17:30:47 +0100

    Splits json view into functions

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const indexView = require('../views/index')
 
 router.route('/')
     .get((req, res) => {
+        res.status(200)
         const view = indexView(req.headers.host)
         res.send(view)
     })
--- views/index.js
+++ views/index.js
@@ -1,31 +1,90 @@
 'use strict'
 
-module.exports = hostname => {
+const getIndexView = hostname => {
     const obj = {
         greeting: 'Welcome to the MessageBoard API!',
-        links: [
-            {
-                rel: 'messages',
-                href: `${hostname}/messages`,
-                action: 'GET',
-            },
-            {
-                rel: 'signup',
-                href: `${hostname}/signup`,
-                action: 'POST',
-            },
-            {
-                rel: 'signin',
-                href: `${hostname}/signin`,
-                action: 'POST',
-            },
-            {
-                rel: 'messages',
-                href: `${hostname}/messages`,
-                action: 'POST'
-            }
-        ]
+        links: []
     }
+
+    obj.links.push(
+        getMessagesGetObj(hostname),
+        getSignUpPostObj(hostname),
+        getSignInPostObj(hostname),
+        getMessagesPostObj(hostname)
+    )
 
     return JSON.stringify(obj)
 }
+
+const getMessagesGetObj = hostname => {
+    // redo
+
+    // have only one for messages with different actions,
+    // see: https://sookocheff.com/post/api/on-choosing-a-hypermedia-format/
+
+    return {
+        rel: messages,
+        href: `${hostname}/${messages}`,
+        action: get
+    }
+}
+
+const getSignUpPostObj = hostname => {
+    return {
+        rel: signUp,
+        href: `${hostname}/${signUp}`,
+        actions: [
+            {
+                class: signUp,
+                href: `${hostname}/${signUp}`,
+                method: post,
+                fields: [
+                    {
+                        name: username,
+                        type: string
+                    },
+                    {
+                        name: password,
+                        type: string
+                    },
+                    {
+                        name: repeatPassword,
+                        type: string
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+const getSignInPostObj = hostname => {
+    // redo
+    return {
+        rel: signIn,
+        href: `${hostname}/${signIn}`,
+        action: post
+    }
+}
+
+const getMessagesPostObj = hostname => {
+    // redo
+    return {
+        rel: messages,
+        href: `${hostname}/${messages}`,
+        action: post
+    }
+}
+
+const get = 'GET'
+const post = 'POST'
+const string = 'string'
+
+const messages = 'messages'
+const signUp = 'sign-up'
+const signIn = 'sign-in'
+
+const username = 'username'
+const password = 'password'
+const repeatPassword = 'repeat-password'
+
+module.exports = getIndexView
